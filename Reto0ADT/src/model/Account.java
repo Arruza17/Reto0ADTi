3a2d9f607b9b67c7b19297acda19fd62331980f0
2dam <2dam@HZ375757.TartangaLH.eus>
2021-09-20 12:15:52 +0200

    Clases hechas

--- Reto0ADT/src/model/Account.java
+++ Reto0ADT/src/model/Account.java
@@ -7,8 +7,103 @@ package model;
 
 /**
  *
- * @author 2dam
+ * @author Ander Arruza
  */
 public class Account {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String middleInitial;
+    private String street;
+    private String city;
+    private String state;
+    private Integer zip;
+    private Integer phone;
+    private String email;
+    //CONSTRUCTOR
+
+    public Account() {
+    }
+    //GETTERS AND SETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
