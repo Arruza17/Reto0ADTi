4afe1832e696af937db6d21e6779b45f727f24e3
2dam <2dam@HZ375749.TartangaLH.eus>
2021-09-22 10:53:19 +0200

    Commit Jorge Menú 22/09/2021 v2

--- Reto0ADT/src/main/Main.java
+++ Reto0ADT/src/main/Main.java
@@ -6,6 +6,13 @@
 package main;
 
 import java.time.Clock;
+import model.AccountControllerIFace;
+import model.AccountControllerImplementationMysql;
+import model.Customer;
+import model.CustomerControllerIFace;
+import model.CustomerControllerImplementationMysql;
+import model.MovementControllerIFace;
+import model.MovementControllerImplementationMysql;
 import resources.Utilidades;
 
 /**
@@ -16,30 +23,90 @@ public class Main {
 
     public static void main(String[] args) {
         //
+        
+        AccountControllerIFace ifaceAcc=new AccountControllerImplementationMysql();
+        CustomerControllerIFace ifaceCus=new CustomerControllerImplementationMysql();
+        MovementControllerIFace ifaceMov=new MovementControllerImplementationMysql();
+        
         int option;
         do{
         System.out.println("Choose an option: \n 1-Create customer \n 2-Consult customer data \n 3-Consult accouts of a client\n 4-Create customer account \n 5-Add customer to an account \n 6-Consult details of an account \n 7-Make movement of an account \n 8-Consult movements of an account \n 9-Exit");
         option=Utilidades.leerInt();
         switch(option){
             case 1:
+                createCus();
                 break;
             case 2:
+                consultCus();
                 break;
             case 3:
+                consultAcc();
                 break;
             case 4:
+                createCus();
                 break;
             case 5:
+                addCus();
                 break;
             case 6:
+                addCusAcc();
                 break;
             case 7:
+                mkMovement();
                 break;
             case 8:
+                consultMv();
                 break;
         }
         if(option>9||option<1){
        System.out.println("Wrong option, please choose an option between 1 to 9"); }
     }while(option!=9);
+    }
+
+    private static void createCus() {
+        Customer cus=new Customer();
+        System.out.println("Insert the following data \n ID of the customer");
+        cus.setId(Utilidades.leerInt());
+        System.out.println("First name");
+        cus.setFirstName(Utilidades.introducirCadena());
+        System.out.println("Last name");
+        cus.setLastName(Utilidades.introducirCadena());
+        System.out.println("Middle Initial");
+        cus.setMiddleInitial(Utilidades.introducirCadena());
+        System.out.println("Street");
+        cus.setStreet(Utilidades.introducirCadena());
+        System.out.println("City");
+        cus.setCity(Utilidades.introducirCadena());
+        System.out.println("State");
+        cus.setCity(Utilidades.introducirCadena());
+        System.out.println("ZIP");
+        cus.setZip(Utilidades.leerInt());
+        System.out.println("Phone");
+        cus.setPhone(Utilidades.leerInt());
+        System.out.println(Utilidades.introducirCadena());
+    }
+
+    private static void consultCus() {
+      System.out.println("Insert the customers ID to consult");
+    }
+
+    private static void consultAcc() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static void addCus() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static void addCusAcc() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static void mkMovement() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static void consultMv() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
